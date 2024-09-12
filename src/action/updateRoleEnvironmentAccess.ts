60f2d63dd0ed8f9ce85f4e259c4e9025dcb28ab2
Wai San Gu <129801024+waisangu@users.noreply.github.com>
2024-09-12 16:38:36 -0400

    [SAZ-369] Fix Contentful Action adding env to Engineering role multiple times (#69)
    * modified updateRoleEnvironmentAccess
    
    * modified updateRoleEnvironmentAccess
    
    * update logging in updateRoleEnvironmentAccess
    
    * refactor to use helper function
    
    * refactor logic inside for loop
    
    * updated updateRoleEnvironmentAccess per feedback
    
    * remove else statement
    
    * reformat

--- src/action/updateRoleEnvironmentAccess.ts
+++ src/action/updateRoleEnvironmentAccess.ts
@@ -1,5 +1,5 @@
 import type { Environment, Space } from 'contentful-management';
-import { createAllowPolicyForEnvironment } from '../role';
+import { containsEnvironmentId, createAllowPolicyForEnvironment } from '../role';
 import { INPUT_UPDATE_ENVIRONMENT_ACCESS_ROLE_ID } from '../constants';
 import { Logger } from '../utils';
 
@@ -8,6 +8,14 @@ export default async function ({ space, environment }: { space: Space; environme
   try {
     Logger.log(`Updating the Role ${roleId} to have access on Environment: ${environment.sys.id}.`);
     const role = await space.getRole(roleId);
+    const isEnvironmentIdInRolePolicy = role.policies.some((policy) =>
+      containsEnvironmentId(policy.constraint, environment.sys.id),
+    );
+    if (isEnvironmentIdInRolePolicy) {
+      Logger.log(`The Role ${roleId} has already been given access to Environment ${environment.sys.id}.`);
+
+      return;
+    }
     role.policies.push(createAllowPolicyForEnvironment(environment.sys.id));
     await role.update();
     Logger.success(`Successfully updated the Role ${roleId} with Environment ${environment.sys.id}.`);
